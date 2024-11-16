098342f87255a9cc1d193fbb7f42d5f0a97f5a52
Dmytro Tsakhlo <dimassjavpg@gmail.com>
2024-11-16 22:49:08 +0200

    cleared db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,22 +1,6 @@
 import {DBType} from "../types/db.type";
 
 export const db: DBType = {
-    blogs: [
-        {
-            "id": "string",
-            "name": "string",
-            "description": "string",
-            "websiteUrl": "string"
-        }
-    ],
-    posts: [
-        {
-            "id": "string",
-            "title": "string",
-            "shortDescription": "string",
-            "content": "string",
-            "blogId": "string",
-            "blogName": "string"
-        }
-    ]
+    blogs: [],
+    posts: []
 }
