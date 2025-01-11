4f310b311ae1581c9552a600ea59b89a08f979a6
Dmytro Tsakhlo <dimassjavpg@gmail.com>
2025-01-11 14:26:46 +0200

    brought out the logic for working with bcrypt into a separate service

--- src/features/auth/bcrypt-service.ts
+++ src/features/auth/bcrypt-service.ts
@@ -1,7 +1,11 @@
-import bcrypt from 'bcrypt';
+import bcrypt, {hash} from 'bcrypt';
 
 export const bcryptService = {
     async checkPassword(password: string, passwordHash: string): Promise<boolean> {
         return bcrypt.compare(password, passwordHash);
-    }
+    },
+    async hashPassword(password: string): Promise<string> {
+        const saltRounds = 10;
+        return await hash(password, saltRounds);
+    },
 };
