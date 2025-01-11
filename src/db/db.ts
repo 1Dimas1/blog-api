3e80c3319b98b86d9a7c299bc706748d25cfd3aa
Dmytro Tsakhlo <dimassjavpg@gmail.com>
2025-01-11 14:23:31 +0200

    added a collection for comments

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,6 +3,7 @@ import {BlogDBType, BlogType} from "../features/blogs/blog.type";
 import {PostDBType, PostType} from "../features/posts/post.type";
 import {SETTINGS} from "../settings";
 import {UserDBType, UserType} from "../features/users/user.type";
+import {CommentType} from "../features/comments/comment.type";
 
 const url = SETTINGS.MONGO_URL
 
@@ -16,6 +17,7 @@ export const database: Db = client.db(SETTINGS.DB_NAME);
 export const blogCollection: Collection<BlogType> = database.collection<BlogType>(SETTINGS.BLOG_COLLECTION_NAME)
 export const postCollection: Collection<PostType> = database.collection<PostType>(SETTINGS.POST_COLLECTION_NAME)
 export const userCollection: Collection<UserType> = database.collection<UserType>(SETTINGS.USER_COLLECTION_NAME)
+export const commentCollection: Collection<CommentType> = database.collection<CommentType>(SETTINGS.COMMENT_COLLECTION_NAME)
 
 export const connectToDB = async () => {
     try {
